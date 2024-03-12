c80808b63c0521f267bfdf7516e690a255163c84
R4NP3R <ssrafa.2014@gmail.com>
2024-03-12 15:54:08 -0300

    ]reflections em Java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.lang.annotation.Annotation;
+
 @Tabela("Tabela Do Hello World")
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Main tabela = new Main();
+        Annotation[] annotations = tabela.getClass().getAnnotations();
+
+        for (Annotation an : annotations) {
+            System.out.println("Anotação: " + an.annotationType());
+            System.out.println("Valor da Anotação: " + tabela.getClass().getAnnotation(Tabela.class));
+        }
     }
+
+
 
 }
